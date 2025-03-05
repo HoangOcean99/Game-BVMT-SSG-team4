5e7137b22e7c3f14daf912911aeb55676fc93bf4
HoangOcean99 <duonghaiduong090905@gmail.com>
2025-03-06 00:22:09 +0700

    UpdateMini2Map2

---
+++ Assets/Script/Map2/Mini2Map2/CountDownTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class CountDownTime : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float timeRemaining = 10f;
+
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        while (timeRemaining > 0)
+        {
+            timerText.text = "0:"+timeRemaining;
+            yield return new WaitForSeconds(1f);
+            timeRemaining--;
+        }
+        timerText.text = "Time's up!";
+    }
+}
---
+++ Assets/Script/Map2/Mini2Map2/InstancePointMini2Map2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class InstancePointMini2Map2 : MonoBehaviour
+{   
+    public static InstancePointMini2Map2 Instance { get; private set; }
+    public int point = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // Xóa instance trùng lặp
+        }
+    }
+}
