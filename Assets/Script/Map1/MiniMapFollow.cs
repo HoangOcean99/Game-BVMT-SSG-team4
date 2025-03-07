56720d069420261364d7f989f2dc68c69e18c766
HoangOcean99 <duonghaiduong090905@gmail.com>
2025-03-07 12:47:48 +0700

    UpdateMiniMap1

---
+++ Assets/Script/Map1/MiniMapFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MiniMapCamera : MonoBehaviour
+{
+    public Transform player; // Nhân vật chính
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 newPosition = player.position;
+            newPosition.z = transform.position.z; // Giữ nguyên giá trị Z để không ảnh hưởng góc nhìn
+            transform.position = newPosition;
+        }
+    }
+} 
