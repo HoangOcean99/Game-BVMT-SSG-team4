75e5268728f85a1518d9cc34297369d1e6f0c341
HoangOcean99 <duonghaiduong090905@gmail.com>
2025-03-09 19:44:09 +0700

    UpdateMap1

---
+++ Assets/Script/Map1/IntroFirstMap1.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroFirstMap1 : MonoBehaviour
+{
+    public TextMeshProUGUI textIntro1;
+    public TextMeshProUGUI textIntro2;
+    public Button buttonTiep;
+    public Button buttonXacNhan;
+    public GameObject panelIntro1;
+    public GameObject panelIntro2;
+
+    private InstanceVariables instanceVariable;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Time.timeScale = 0;
+        instanceVariable = FindAnyObjectByType<InstanceVariables>();
+        textIntro1.text = "Xin chào kĩ sư môi trường " + instanceVariable.nameUser + ", thành phố của bạn đang bị ô nhiễm rất trầm trọng, người dân đã di tản. <sprite=2>";
+        textIntro2.text = "Nhiệm vụ của bạn là hoàn thành các minigame và câu hỏi để mang lại sự trong lành và bình yên cho thành phố. <sprite=3>";
+
+        buttonTiep.onClick.AddListener(() =>
+        {
+            panelIntro1.SetActive(false);
+            panelIntro2.SetActive(true);
+        });
+        buttonXacNhan.onClick.AddListener(() =>
+        {
+            panelIntro2.SetActive(false);
+            Time.timeScale = 1;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
